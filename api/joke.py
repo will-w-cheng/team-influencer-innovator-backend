5ebfcef15439ef72d2bbcad65041a7634011ff7b
Saaras859 <kodalisaaras@gmail.com>
2023-10-18 11:03:22 -0700

    Fixed the sqlite database bug

--- api/joke.py
+++ api/joke.py
@@ -61,7 +61,7 @@ class JokesAPI:
     api.add_resource(_UpdateJeer, '/jeer/<int:id>')
     
 if __name__ == "__main__": 
-    # server = "http://127.0.0.1:5000" # run local
+    #server = "http://127.0.0.1:5000" # run local
     server = 'https://flask.nighthawkcodingsociety.com' # run from web
     url = server + "/api/jokes"
     responses = []  # responses list

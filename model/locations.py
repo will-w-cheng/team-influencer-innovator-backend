32c882d03da92dcdaa89e24d59d1c88b8fa6922d
will-w-cheng <will.wi.cheng@gmail.com>
2023-10-23 20:34:48 -0700

    Verbose commenting onto locations, modify the users api for the leaderboard
    - Table name is leaderboard, 3 keys within the model defined to be id, username, and points
    
    - Initialize class initializes the username and points
    - Added a points setter attribute and ensured checking for points to be above, and added update points + sample user data for now to test

--- model/locations.py
+++ model/locations.py
@@ -2,15 +2,25 @@ from __init__ import db
 # from flask import render_template  # import render_template from "public" flask libraries
 # app = flask(__name__)
 class Location(db.Model):
+
+    # Ok so you basically create the new table for the database as the location
     __tablename__ = "locations"
+
+    # Adding the ID field for the table name of locations
     id = db.Column(db.Integer, primary_key=True)
+
+    # Adding the location_name field to the table name of locations in the sqlite db
     location_name = db.Column(db.String, nullable=False)
+
+    # Adding the image tag which will be parse in data as a string which Saaras will input as base64
     image = db.Column(db.String, nullable=False)
 
+    # Class of init location names and images
     def __init__(self, location_name, image):
         self.location_name = location_name
         self.image = image
 
+    # Convert the the db fields into a dictionary where you can access it
     def to_dict(self):
         return {"id": self.id, "location_name": self.location_name, "image": self.image}
 
@@ -31,6 +41,6 @@ def init_locations():
     db.session.commit()
 
 if __name__ == '__main__':
-    # Initialize your database with the application context
+    # Initialize your database with the application context, so  that it can create it based on the things
     with db.create_scoped_session() as session:
         init_locations()

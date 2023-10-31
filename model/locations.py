2fbe3b0c41ccc494b69f80d41c76a8e2a04b53ef
will-w-cheng <77639521+will-w-cheng@users.noreply.github.com>
2023-10-31 10:40:36 -0700

    Added more to the database for base64 and added functionality

--- model/locations.py
+++ model/locations.py
@@ -1,5 +1,6 @@
 from __init__ import db
 import base64
+import os
 
 class Location(db.Model):
     __tablename__ = "locations"
@@ -23,22 +24,52 @@ def image_to_base64(image_path):
         return base64_string
 
 # Example usage
-image_path = "static/assets/IMG_0902.jpg"  # Relative path to the image
-base64_data = image_to_base64(image_path)
-image_path2 = "static/assets/IMG_0908.jpg"  # Another relative path to a different image
-base64_data2 = image_to_base64(image_path2)
+image_path = "static/assets/images/"  # Relative path to the image
 # locationposition=(20, 45)
 # AND locationposition=(20, 45)
 def init_locations():
-    location1 = Location(location_name="20, 45", image=base64_data)
-    location2 = Location(location_name="35, 40", image=base64_data2)
-    # location3 = Location(location_name="Location 3", image="image3.jpg")  # Placeholder for the third image
+    # Initialize data for now just from lists but later we push it into a database
+    b64_lst = []
+    '''
+    If you want to add an image you can just put it in the static/assets/images
+    But also append your coordinates to the following list below 
+    '''
+    coordinates_data = ["20, 45", "35, 40"]
 
-    db.session.add(location1)
-    db.session.add(location2)
-    # db.session.add(location3)
 
-    db.session.commit()
+    '''
+    Now from here what we do is we loop through all the filenames and the image_path and then append them to lists so it's easy to just loop through later
+    and then add them to a session
+    '''
+    count = 0
+    for filename in os.listdir(image_path):
+        if os.path.isfile(os.path.join(image_path, filename)):
+            count += 1
+            b64_lst.append(image_to_base64(image_path + filename))
+
+
+    #### Add stuff to the db now
+    for b64_image in range(0, len(b64_lst)):
+        location = Location(location_name=coordinates_data[b64_image], image=b64_lst[b64_image])
+        db.session.add(location)
+        db.session.commit()
+
+    
+
+    '''
+    Old code for this location 
+    '''
+
+
+    # location1 = Location(location_name="20, 45", image=base64_data)
+    # location2 = Location(location_name="35, 40", image=base64_data2)
+    # # location3 = Location(location_name="Location 3", image="image3.jpg")  # Placeholder for the third image
+
+    # db.session.add(location1)
+    # db.session.add(location2)
+    # # db.session.add(location3)
+
+    # db.session.commit()
 
 if __name__ == '__main__':
     init_locations()

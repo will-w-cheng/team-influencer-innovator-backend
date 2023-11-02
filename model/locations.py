8b7124b3d71b3db82c3d4a9709e753af0fbee3ac
Saaras859 <kodalisaaras@gmail.com>
2023-11-01 23:03:31 -0700

    Updated it potentially testing if it will work

--- model/locations.py
+++ model/locations.py
@@ -34,7 +34,7 @@ def init_locations():
     If you want to add an image you can just put it in the static/assets/images
     But also append your coordinates to the following list below 
     '''
-    coordinates_data = ["250, 500", "100, 450", "700, 250", "76, 650", "150, 0"]
+    coordinates_data = ["250, 500", "100, 450", "412, 620", "76, 650", "176, 700"]
 
 
     '''
